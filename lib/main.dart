1c7405953ff0f5a8c36d5f1004286d675cfd7bd6
achaudhary591 <achaudhary591@gmail.com>
2023-03-22 10:56:12 +0530

    did implemented bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:bloc_architecture/login_view.dart';
 import 'package:flutter/material.dart';
+
+import 'counter_bloc.dart';
+import 'counter_event.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,27 +16,37 @@ class MyApp extends StatelessWidget {
       title: 'BLoc Architecture',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // useMaterial3: true,
-        /*colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.red
-        ),*/
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: LoginView(),
+      home: const MyHomePage(title: 'BLoc Architecture home page'),
     );
   }
 }
 
-/*class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
-}*/
+}
 
-/*class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> {
+  final _bloc = CounterBloc();
+
+  /*int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;/*int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
@@ -46,7 +58,9 @@ class MyApp extends StatelessWidget {
     setState(() {
       _counter--;
     });
-  }
+  }*/
+    });
+  }*/
 
   @override
   Widget build(BuildContext context) {
@@ -55,24 +69,30 @@ class MyApp extends StatelessWidget {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: StreamBuilder(
+          stream: _bloc.counter,
+          initialData: 0,
+          builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '${snapshot.data}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
+            );
+          },
         ),
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
-            onPressed: _incrementCounter,
+            onPressed: () => _bloc.counterEventSink.add(IncrementEvent()),
             tooltip: 'Increment',
             child: const Icon(Icons.add),
           ),
@@ -80,7 +100,7 @@ class MyApp extends StatelessWidget {
             width: 10,
           ),
           FloatingActionButton(
-            onPressed: _decrementCounter,
+            onPressed: () => _bloc.counterEventSink.add(DecrementEvent()),
             tooltip: 'Decrement',
             child: const Icon(Icons.remove),
           ),
@@ -88,4 +108,11 @@ class MyApp extends StatelessWidget {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-}*/
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _bloc.dispose();
+  }
+}
